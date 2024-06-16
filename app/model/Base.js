638b96057d97b92221f2db89982fc6c1283c912e
Tommy Son <tommy.m.son@gmail.com>
2024-06-15 19:57:11 -0700

    Run 'sencha generate app --ext MatchaOverSencha ./MatchaOverSencha'

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('MatchaOverSencha.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'MatchaOverSencha.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('MatchaOverSencha.model.Personnel', {
+    extend: 'MatchaOverSencha.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
